91f5125845eb48121a7c0d2c0f781cbed85cb166
deybyr647 <deybyr647@gmail.com>
2020-08-15 12:30:08 -0400

    Created minimal http server using Express.js & Node.js. Tested with personal website files included in the client folder

---
+++ expressLocalServer.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let app = express();
+let opn = require('opn');
+
+app.use(express.static('client'));
+
+let server = app.listen(8080, () => {
+    let host = server.address().address;
+    let port = server.address().port;
+
+    console.log(`Express App listening at http://localhost:${port}`);
+})
+
+opn('http://localhost:8080');
+
