4209a5458f679f2a4bb5a530f60fe99182b5c7a5
Ian Graham Martinez <Ian.graham28@gmail.com>
2020-08-15 16:28:44 -0400

    -

--- hackathon-rust/src/main.rs
+++ hackathon-rust/src/main.rs
@@ -56,34 +56,12 @@ fn send_notif(title : &str, body : &str, icon : &str) {
 		.unwrap();
 }
 
-fn get_pred () -> String {
+fn get_pred () {
 	let response = call_node_string("readFace");
-	String::from(response.split("|").nth(1).unwrap())
 }
 
-fn face_data() -> json::JsonValue {
-	use std::time::*;
-	let time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
-	let prediction = get_pred();
-	let active_app = call_python_string("../python/activeapp.py");
 
-	let mut object = json::object! {
-		time : 0,
-		prediction : "",
-		active_app : ""
-	};
-
-	object["time"] = time.into();
-	object["prediction"] = prediction.replace("\\\"", "\"").into();
-	object["active_app"] = active_app.into();
-
-	object
-}
 
 fn main() {
-	use std::io::*;
-	let mut output_file = std::fs::File::create("../output.txt").unwrap();
-	let face_data = face_data();
-	let mut data = face_data.as_str().unwrap();
-	output_file.write(unsafe { data.as_bytes_mut() }).unwrap();
+
 }
