9416170ea4bd6cb851d252aa9e5daeb099978340
deybyr647 <deybyr647@gmail.com>
2020-08-16 00:00:10 -0400

    Added some client side code which displays text regarding the predicted mood

--- client/moodcam.js
+++ client/moodcam.js
@@ -1,14 +1,46 @@
+const moodHeading = document.querySelector('#mood');
+
+let capitalizeStr = (str) => {
+    let splitStr = str.toLowerCase().split(' ');
+    for(let l = 0; l < splitStr.length; l++){
+        splitStr[l] = splitStr[l].charAt(0).toUpperCase() + splitStr[l].substring(1);
+    }
+
+    return splitStr.join(' ');
+}
+
+let getMood = (arr) => {
+    let finalMoodResult;
+    arr.forEach(result => {
+        Object.keys(result.data).reduce((a, b) => {
+            if(result.data[a] > result.data[b]){
+                finalMoodResult = a;
+            }
+        })
+        console.log(result.data);
+    })
+
+    console.log('Final Mood Result: ', finalMoodResult);
+    return finalMoodResult;
+}
+
+let displayMoodData = () => {
+
+}
+
 //Fetches data file into the browser
-let fetchData = (file) => {
+let getData = (file) => {
     fetch(file)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            console.log('Done!')
+            let objArr = data.results;
+            getMood(objArr);
+            let mood = getMood(objArr);
+            moodHeading.innerHTML = `You seem pretty ${capitalizeStr(mood)}`;
         })
         .catch(err => {
             err ? console.error(err) : err = null;
         })
 }
 
-fetchData('results.json');
+getData('results.json');
