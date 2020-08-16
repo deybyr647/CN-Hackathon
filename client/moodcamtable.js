2bcb5da3ec95aa4745a8672ed50ead5d6c665455
Ian Graham Martinez <Ian.graham28@gmail.com>
2020-08-16 10:32:55 -0400

    -

--- client/moodcamtable.js
+++ client/moodcamtable.js
@@ -1,5 +1,18 @@
-var json = require("results.json")
-var results = json.results;
-for(var i = 0; i < results.len(); i++) {
-	
+var results = data.results;
+var table = document.getElementById("table");
+for(var i = 0; i < results.length; i++) {
+	var entry = results[i];
+	var row = document.createElement("tr");
+
+	var time_td = document.createElement("td");
+	var time = document.createTextNode(entry.time);
+	time_td.appendChild(time);
+	row.appendChild(time_td);
+
+	var app_td = document.createElement("td");
+	var app = document.createTextNode(entry.app);
+	app_td.appendChild(app);
+	row.appendChild(app_td);
+
+	table.appendChild(row);
 }
